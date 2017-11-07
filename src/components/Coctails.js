e9d244931e26b0b3f29ef82c0f56e4871044d6c0
Vlad Tkachov <vlad.miakshin@gmail.com>
2017-11-07 02:28:47 +0200

    added function for show coctails

--- src/components/Coctails.js
+++ src/components/Coctails.js
@@ -3,38 +3,53 @@ import React from 'react';
 const base =[
   {
     name: 'Negroni',
-    img : '#',
-    components: {
-      'Джин': '30ml',
-      'Красный вермут Martini':'30ml',
-      'Кампари':'30ml',
-      'Апельсин':'30g',
-      'Лед в кубиках':'120g'
-    },
-    recipe : ' Наполни рокс кубиками льда доверху
-    Налей в бокал красный вермут 30 мл и кампари 30 мл
-    Добавь джин 30 мл и размешай коктейльной ложкой
-    Укрась кружком апельсина '
+    img : '../img/someCoct.png',
+    components: [
+      'Джин: 30ml',
+      'Красный вермут Martini:30ml',
+      'Кампари:30ml',
+      'Апельсин:30g',
+      'Лед в кубиках:120g']
+    ,
+    recipe : 'Наполни рокс кубиками льда доверху. Налей в бокал красный вермут 30 мл и кампари 30 мл. Добавь джин 30 мл и размешай коктейльной ложкой.Укрась кружком апельсина. '
   },
   {
     name: 'Daykiry',
-    img : '#',
-    components: {
-      'Белый ром': '60ml',
-      'Сахарный сироп':'15ml',
-      'Лаймовый сок':'30ml',
-      'Лед в кубиках':'200g'
-    },
-    recipe : 'Налей в шейкер лаймовый сок 30 мл, сахарный сироп 15 мл и белый ром 60 мл
-    Наполни шейкер кубиками льда и взбей
-    Перелей через стрейнер в охлажденное шампанское блюдце'
+    img : 'img/someCoct.png',
+    components: [
+      'Белый ром: 60ml',
+      'Сахарный сироп:15ml',
+      'Лаймовый сок:30ml',
+      'Лед в кубиках:200g'
+    ],
+    recipe : 'Налей в шейкер лаймовый сок 30 мл, сахарный сироп 15 мл и белый ром 60 мл. Наполни шейкер кубиками льда и взбей.Перелей через стрейнер в охлажденное шампанское блюдце.'
   }
 ]
 
-const Coctails = () => (
-  <div>
-    <h1>In this page i'll be render coctails</h1>
-  </div>
-  );
+class Coctails extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {store: base
+    };
+  }
+  render(){
+    const showCoctails = this.state.store.map((coctail,id) =>
+    <article key={id}>
+      <img src={coctail.img}/>
+      <p>Название: {coctail.name}</p>
+      <p>Компоненты: {
+        coctail.components.map((item,id)=>
+      <li key={id}>{item}</li>
+    )
+      }</p>
+      <p>Рецепт приготовления: {coctail.recipe}</p>
+    </article>
+  )
+
+    return(
+      <div>{showCoctails}</div>
+    )
+  }
+}
 
 export default Coctails;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () =>
-<nav>
+<nav className="navigation">
   <li><Link to='/'>Home</Link></li>
   <li><Link to='/Coctails'>Coctails</Link></li>
   <li><Link to='/AddCoctail'>Add coctail</Link></li>
