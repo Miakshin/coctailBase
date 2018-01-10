cae8ac5250406cd1a27c2e9250035df0b28ad2c3
Vlad Tkachov <vlad.miakshin@gmail.com>
2018-01-10 16:18:02 +0200

    home page is dan

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,17 @@ import './Footer.css';
 
 const Footer = () =>
 <footer className="footer">
-  <p>Create by Vlad Tkachov</p>
+  <p>The application was created as part of the portfolio by Vlad Tkachov</p>
+  <address>
+  <p>Contact me:<br/>
+    <span>phon number: +380938867784</span>
+  </p>
+  <div className="links">
+    <a href="https://github.com/Miakshin/fileUploader" className="link-to-git"></a>
+    <a href="https://www.facebook.com/profile.php?id=1450125752" className="link-to-fb"></a>
+    <a href="https://dou.ua/users/vlad-tkachyov/" className="link-to-dou" ></a>
+  </div>
+  </address>
 </footer>
 
 export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,8 +8,9 @@ const Home = () => (
     <div className="homeWrapper">
     <h1>Simplу Recipe Collection</h1>
       <p>
-      Welcome to big coctail collection. Today you have the opportunity enjoy cocktails from our collection and spend time with pleasure.
-      Transitions to our recipe store to find something to taste.<br/>
+      <h2>Cocktails are mysterious drinks. It is necessary to try a lot to choose the one, to your liking. And its quality depends on the skill of the bartender.</h2>
+      <span>Welcome to big coctail collection. Today you have the opportunity enjoy cocktails from our collection and spend time with pleasure.
+      Transitions to our recipe store to find something to taste.</span><br/>
       <Link to='/Coctails'>Let`s try</Link>
       </p>
     </div>
